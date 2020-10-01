80cb37c598efd50962c36c5dbbd6faba2f0a02d6
fweak1337 <freakgamer2331@gmail.com>
2020-10-01 15:17:36 -0700

    Update ex.js

--- JavaScript/ex.js
+++ JavaScript/ex.js
@@ -2,8 +2,5 @@ const { Webhook } = require("./main");
 
 const webhook = new Webhook("704517448677851226", "TDlPm0x-SOME-xq3JGubkMYS3Ks7JnCOOOOLfygFGZ6XJPCHHifTKN");
 
-( async function() {
-
-    await webhook.send({ content: "Testing", embed: { description: "Testing"}})
-
-})();
+webhook.send({ content: "Testing", embed: { description: "Testing"}})
+.then( (result) => result ? console.log('Sent webhook message') : console.log('Error occured someone') )
